52ec6c21057505229ae86e29893d0b71a8ca7770
cheolubak <dev.cheolubak@gmail.com>
2022-10-03 21:10:14 +0900

    Home Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/pages/Home';
 
 function App() {
-  return <h1>Hello World!</h1>;
+  const router = createBrowserRouter([{ path: '/', element: <Home /> }]);
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/components/pages/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../../molecules/Header';
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
