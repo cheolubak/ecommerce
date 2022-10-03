e540e7dd93249445b4d5729921ce4a1ee3e968c7
Cheolu B <b.angelthink@gmail.com>
2022-10-03 14:10:44 +0900

    Change button to forwardRef

--- src/components/atom/Button/Button.tsx
+++ src/components/atom/Button/Button.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { ButtonProps } from './ButtonProps';
 import { ButtonStyled } from './ButtonStyled';
 
-export default function Button({
-  color = 'primary',
-  size = 'large',
-  shape = 'fill',
-  ...props
-}: ButtonProps) {
-  return (
-    <ButtonStyled
-      color={color}
-      size={size}
-      shape={shape}
-      {...props}
-    >
-      {props.children}
-    </ButtonStyled>
-  );
-}
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ color = 'primary', size = 'large', shape = 'fill', ...props }, ref) => {
+    return (
+      <ButtonStyled
+        ref={ref}
+        color={color}
+        size={size}
+        shape={shape}
+        {...props}
+      >
+        {props.children}
+      </ButtonStyled>
+    );
+  }
+);
+
+export default Button;
