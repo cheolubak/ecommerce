01ec9e836d22564b91edb5152ac1e311de4ffb27
cheolubak <dev.cheolubak@gmail.com>
2022-10-04 22:43:55 +0900

    Add Container

---
+++ src/components/atoms/Container/ContainerStyled.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const ContainerStyled = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+
+  @media screen and (max-width: 1440px) {
+    width: 100vw;
+  }
+`;
