2786d3c0f7af519dd1c6b288801b18756d17ba1e
cheolubak <dev.cheolubak@gmail.com>
2022-10-04 22:49:00 +0900

    Use Recoil

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import React from 'react';
+import { RecoilRoot } from 'recoil';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './components/pages/Home';
 
 function App() {
   const router = createBrowserRouter([{ path: '/', element: <Home /> }]);
-  return <RouterProvider router={router} />;
+  return (
+    <RecoilRoot>
+      <RouterProvider router={router} />
+    </RecoilRoot>
+  );
 }
 
 export default App;
