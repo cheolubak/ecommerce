e629297b2a0c218d7aca86fde6152eecf751cf28
cheolubak <dev.cheolubak@gmail.com>
2022-10-04 22:45:08 +0900

    Fetch image with cloud storage

--- src/util/Firebase/Storage.ts
+++ src/util/Firebase/Storage.ts
@@ -1,5 +1,10 @@
 import Firebase from './Firebase';
+import { ref, getDownloadURL } from 'firebase/storage';
 
-class Storage extends Firebase {}
+class Storage extends Firebase {
+  static fetchImage(parent: string, path: string) {
+    return getDownloadURL(ref(this.storage, `${parent}/${path}`));
+  }
+}
 
 export default Storage;
