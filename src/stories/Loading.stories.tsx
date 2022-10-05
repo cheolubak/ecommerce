9d3d8acde8365c89e9862116b03f8827c307f01d
cheolubak <dev.cheolubak@gmail.com>
2022-10-05 21:07:38 +0900

    Add Loading Storybook

---
+++ src/stories/Loading.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import Loading from '../components/atoms/Loading';
+
+export default {
+  title: 'Components/Loading',
+  component: Loading,
+  argTypes: {},
+} as ComponentMeta<typeof Loading>;
+
+const Template: ComponentStory<typeof Loading> = (args) => (
+  <Loading {...args} />
+);
+
+export const Default = Template.bind({});
+Default.args = {};
